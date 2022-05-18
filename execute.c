e91fd3a931edbc4fef6ae6f7d33f83055e6c655a
Timmy <emmatimmybello@gmail.com>
2022-05-18 22:46:16 +0100

    Made changes to code and file names

--- execute.c
+++ execute.c
@@ -1,32 +1,115 @@
 #include "shell.h"
-/**
- * exec - Creates a new child process,
- * executes a command and wait for the child process
- * to update the status
- * @arguments: Array of inputs by the user
- * Return: 0 if success
- */
-int exec(char **arguments)
-{
-	pid_t pid = 0;/**Child process id*/
-	int stat = 0, exe_stat = 0;/**indicates the status of the child process*/
 
-	pid = fork();/**Create a child process*/
-	if (pid == -1)/**Failed to create*/
-		_printp("failed\n", 7);
-	else if (pid == 0)/**He is the son...*/
+/**
+ * execute - A function that executes a command.
+ * @command: The pointer to tokienized command
+ * @name: The name of the shell.
+ * @env: The pointer to the enviromental variables.
+ * @cicles: Number of executed cicles.
+ * Return: Nothing.
+ */
+void execute(char **command, char *name, char **env, int cicles)
+{
+	char **pathways = NULL, *full_path = NULL;
+	struct stat st;
+	unsigned int i = 0;
+
+	if (_strcmp(command[0], "env") != 0)
+		print_env(env);
+	if (stat(command[0], &st) == 0)
 	{
-		exe_stat = execve(arguments[0], arguments, environ);/**Run the command*/
-		if (exe_stat == -1)
+		if (execve(command[0], command, env) < 0)
 		{
-			exe_stat = 126;
-			perror("hsh");
-			exit(exe_stat);
-		} /**Finish the child process successfully*/
-		exit(0);
+			perror(name);
+			free_exit(command);
+		}
 	}
-	else /**He is the father*/
-		wait(&stat);/**Stops the execution of the parent until the child ends*/
-	exe_stat = WEXITSTATUS(stat);
-	return (exe_stat);
+	else
+	{
+		pathways = _getPATH(env);
+		while (pathways[i])
+		{
+			full_path = _strcat(pathways[i], command[0]);
+			i++;
+			if (stat(full_path, &st) == 0)
+			{
+				if (execve(full_path, command, env) < 0)
+				{
+					perror(name);
+					free_dp(pathways);
+					free_exit(command);
+				}
+				return;
+			}
+		}
+		msgerror(name, cicles, command);
+		free_dp(pathways);
 	}
+}
+
+
+/**
+ * print_env - A function that prints all enviromental variables.
+ * @env: The pointer to enviromental variables.
+ * Return: Nothing.
+ */
+void print_env(char **env)
+{
+	size_t i = 0, len = 0;
+
+	while (env[i])
+	{
+		len = _strlen(env[i]);
+		write(STDOUT_FILENO, env[i], len);
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+}
+
+
+/**
+ * _getPATH - A function to gets the full value from.
+ * enviromental variable PATH.
+ * @env: The pointer to enviromental variables.
+ * Return: All tokenized pathways for commands.
+ */
+char **_getPATH(char **env)
+{
+	char *pathvalue = NULL, **pathways = NULL;
+	unsigned int i = 0;
+
+	pathvalue = strtok(env[i], "=");
+	while (env[i])
+	{
+		if (_strcmp(pathvalue, "PATH"))
+		{
+			pathvalue = strtok(NULL, "\n");
+			pathways = tokening(pathvalue, ":");
+			return (pathways);
+		}
+		i++;
+		pathvalue = strtok(env[i], "=");
+	}
+	return (NULL);
+}
+
+
+/**
+ * msgerror - A function that prints message not found.
+ * @name: The name of the shell.
+ * @cicles: Number of cicles.
+ * @command: The pointer to tokenized command.
+ * Return: Nothing.
+ */
+void msgerror(char *name, int cicles, char **command)
+{
+	char c;
+
+	c = cicles + '0';
+	write(STDOUT_FILENO, name, _strlen(name));
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, &c, 1);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, command[0], _strlen(command[0]));
+	write(STDOUT_FILENO, ": not found\n", 12);
+}
--- shell.h
+++ shell.h
@@ -1,34 +1,54 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-extern char **environ;
 
-int _strcmp(char *str1, char *str2);
-int verify_path(char **arguments);
-char *_strdup(char *str);
-char *append_command(char *dir_path, char *command);
-int exec(char **arguments);
-int fill_args(char *entry, char **arguments);
-int print_numbers(int n);
-int _strlen(const char *string);
-int print_not_found(char **arguments, int counter);
-char *_getenv(char *global_var);
-int _printp(const char *prompt, unsigned int size);
-int _putchar(char c);
-int exist(char *pathname);
-void free_grid(char **grid, int heigth);
-void last_free(char *entry);
-int verify_blt(char **arguments, int exit_stat);
+/*============     Shell_Init       ==============*/
 
-#endif
+int main(int ac, char **av, char **env);
+void prompt(void);
+void handle(int signals);
+void _EOF(char *buffer);
+void shell_exit(char **command);
 
+
+/*============     create_child       ==============*/
+
+void create_child(char **command, char *name, char **env, int cicles);
+int change_dir(const char *path);
+
+/*============        Execute       ==============*/
+
+void execute(char **command, char *name, char **env, int cicles);
+void print_env(char **env);
+char **_getPATH(char **env);
+void msgerror(char *name, int cicles, char **command);
+
+/*============          Tokening      ==============*/
+
+char **tokening(char *buffer, const char *s);
+
+
+/*============     Free Memory      ==============*/
+
+
+void free_dp(char **command);
+void free_exit(char **command);
+
+/*============  Auxiliar_Functions    ==============*/
+
+int _strcmp(char *s1, char *s2);
+unsigned int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+
+/*============ END      ==============*/
+
+#endif /* _SHELL_H_ */
